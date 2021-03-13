d55acb8d45c1fb294261a1f83aa35c85d97749c9
shanpeshwe <shantanupeshwe@gmail.com>
2021-03-13 15:37:43 +0530

    basic c programs

---
+++ C_Programs/InputDecimalNumberAndPrint.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ float num1;
+ clrscr();
+ printf("enter decimal number : ");
+ scanf("%f",&num1);
+ printf("\ndecimal number is : %f ",num1);
+ getch();
+}
---
+++ C_Programs/MultiplicationOf3Numbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int num1,num2,num3;
+ clrscr();
+ printf("enter number 1 : ");
+ scanf("%d",&num1);
+ printf("\nenter number 2 : ");
+ scanf("%d",&num2);
+ printf("\nenter number 3 : ");
+ scanf("%d",&num3);
+ printf("\nmultiplication of 3 numbers is = %d",num1*num2*num3);
+ getch();
+}
