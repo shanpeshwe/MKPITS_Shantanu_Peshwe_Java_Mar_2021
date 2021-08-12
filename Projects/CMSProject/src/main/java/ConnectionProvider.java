5a314e118e42281c48b5c81c7afb6e779b6bc307
Harshal Danao <hdanao86@gmail.com>
2021-08-11 22:21:06 -0700

    updated changes

---
+++ Projects/CMSProject/src/main/java/ConnectionProvider.java
@@ -0,0 +1,23 @@
+package bean;  
+    import java.sql.*;  
+    import static bean.Provider.*;  
+      
+    public class ConnectionProvider {  
+    private static Connection con=null;  
+    static{  
+    try{  
+    Class.forName(DRIVER);  
+    con=DriverManager.getConnection(CONNECTION_URL,USERNAME,PASSWORD);  
+    }catch(Exception e)
+{
+System.out.println(e.toString());
+}  
+    
+    
+    }  
+      
+    public static Connection getCon(){  
+        return con;  
+    }  
+      
+    }  
